3fad3579119de59d13a968c8c81002f35e813ccf
Shawn Santiago <santiagoshawn@gmail.com>
2016-04-11 14:51:51 -0400

    first commit

---
+++ bin/server.js
@@ -0,0 +1,29 @@
+var five = require("johnny-five"),
+    express = require("express"),
+    app = express(),
+    http = require("http"),
+    server = require('http').Server(app);
+    io = require('socket.io')(server);
+
+app.use(express.static('public'));
+server.listen(8082, function(){
+  console.log('listening on *:3000');
+});
+
+var board = new five.Board({port: '/dev/cu.usbmodem1411'});
+
+io.on('connection', function(socket){
+  console.log('Connected');
+  board.on("ready", function() {
+    console.log('Board Ready');
+    var sensor = new five.Sensor({
+      pin: "A0",
+      freq: 200
+    });
+
+    sensor.scale([ 0, 100 ]).on("change", function() {
+      console.log(this.value);
+      socket.emit('pulse', this.scaled)
+    });
+  });
+});
