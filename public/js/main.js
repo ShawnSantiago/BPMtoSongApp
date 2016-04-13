b91f703accf3d08e8c1cc44f2a0d41883b00e091
Shawn Santiago <santiagoshawn@gmail.com>
2016-04-13 19:07:08 -0400

    add settings page

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,7 @@ $(function () {
       pulse_data = [],
       pulse_avg = [],
       totalPoints = 100,
+      sethrIncrease = 20,
       heart_rate = 0;
 
   socket.on('pulse', function (data) {
@@ -15,8 +16,12 @@ $(function () {
       for (var i = 0; i < pulse_avg.length; i++) {
         peak_sum += pulse_avg[i];
       }
-      heart_rate = parseInt((peak_sum / pulse_avg.length), 10);
-      $('#heartrate').html(heart_rate+20);
+      heartRate = parseInt((peak_sum / pulse_avg.length), 10);
+
+      heart_rate = heartRate
+      $('#heartrate').html(heart_rate+sethrIncrease);
+      
+      
     }
 
   });
@@ -26,47 +31,68 @@ $(function () {
     pulse_data.push(0);
   }
 
-});
 
 // soundcloud
 
-SC.initialize({
-      client_id: "51a05ad7e3e35bef46da014ffdfa38b5",
-      redirect_uri: "localhost:8000",
+  SC.initialize({
+        client_id: "51a05ad7e3e35bef46da014ffdfa38b5",
+        redirect_uri: "localhost:8000",
+
+    });
+
+  function getTracks(){
+   console.log("from: " + (heart_rate+sethrIncrease) + " " + "to: " + (heart_rate+sethrIncrease)) 
+    SC.get("/tracks", {
+        bpm:{
+          from:heart_rate+sethrIncrease,
+          to: heart_rate+sethrIncrease
+        },
+        limit: 10
+      }, function (tracks) {
+
+          var trackList = [];
+
+          for (var i = 0; i < tracks.length; i++) {
+
+              trackList.push(tracks[i].permalink_url);
+
+          }
+          embedPlayer(trackList);
+      });
+  }
+
+  function embedPlayer (trackListArray) {
+    SC.oEmbed(trackListArray[0]// user or playlist to embed
+      , { color: "000"
+        , auto_play: true
+        , maxwidth: 800
+        , maxheight: 1000
+        , show_comments: true } // options
+      , document.getElementById("widget") // what element to attach player to
+    );
+  }
+
+  $( "#button" ).click(function() {
+    getTracks();
+  });
+
+  $("a[rel*=leanModal]").leanModal({ closeButton: ".modal_close" });
+
+  $( "#slider-setHeartRate-min" ).slider({
+        range: "min",
+        value: 20,
+        min: 1,
+        max: 200,
+        slide: function( event, ui ) {
+          $( "#amountHR" ).val( "HR " + ui.value );
+        }
+      });
+  $( "#amountHR" ).val( "HR " + $( "#slider-setHeartRate-min" ).slider( "value" ) );
+
+  $( "#set" ).click(function() {
+    sethrIncrease = $( "#slider-setHeartRate-min" ).slider( "value" )
 
   });
 
-function getTracks(){
-  SC.get("/tracks", {
-      bpm:{
-        from:100,
-        to: 150
-      },
-      limit: 10
-    }, function (tracks) {
+});
 
-        var trackList = [];
-
-        for (var i = 0; i < tracks.length; i++) {
-
-            trackList.push(tracks[i].permalink_url);
-
-        }
-        embedPlayer(trackList);
-    });
-}
-
-    function embedPlayer (trackListArray) {
-      SC.oEmbed(trackListArray[0]// user or playlist to embed
-        , { color: "000"
-          , auto_play: true
-          , maxwidth: 800
-          , maxheight: 1000
-          , show_comments: true } // options
-        , document.getElementById("widget") // what element to attach player to
-      );
-    }
-
-    window.onload = function() {
-      getTracks();
-    };
