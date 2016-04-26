e659b7e2b13f9c6ca955e9568faa90784c25d47f
Shawn Santiago <santiagoshawn@gmail.com>
2016-04-25 23:40:49 -0400

    styling

--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,7 @@ $(function () {
       sethrIncrease = 20,
       heart_rate = 0;
 
-  socket.on('pulse', function (data) {
+   socket.on('pulse', function (data) {
 
     pulse_avg.push(data);
     if (pulse_avg.length >= 10) {
@@ -19,7 +19,6 @@ $(function () {
       heartRate = parseInt((peak_sum / pulse_avg.length), 10);
 
       heart_rate = heartRate
-      $('#heartrate').html(heart_rate+sethrIncrease);
       
       
     }
@@ -40,40 +39,51 @@ $(function () {
 
     });
 
-  function getTracks(){
-   console.log("from: " + (heart_rate+sethrIncrease) + " " + "to: " + (heart_rate+sethrIncrease)) 
+  function getTracks(genreId){
     SC.get("/tracks", {
+        //genres: genreId,
         bpm:{
-          from:heart_rate+sethrIncrease,
-          to: heart_rate+sethrIncrease
+          from: heart_rate-5+sethrIncrease,
+          to: heart_rate+5+sethrIncrease
         },
-        limit: 10
+        limit: 10,
+
       }, function (tracks) {
-
+          console.log(tracks);
           var trackList = [];
-
           for (var i = 0; i < tracks.length; i++) {
-
               trackList.push(tracks[i].permalink_url);
-
           }
+
+          console.log(heart_rate + sethrIncrease);
+          $("#heart").hide();
+          var bpm = heart_rate + sethrIncrease;
+          $('#heartrate_container').show();
+          $('#heartrate').html("BPM: " + bpm);
           embedPlayer(trackList);
       });
   }
 
+
+
   function embedPlayer (trackListArray) {
-    SC.oEmbed(trackListArray[0]// user or playlist to embed
+    trackNumber = Math.floor((Math.random() * trackListArray.length) + 1);
+    SC.oEmbed(trackListArray[trackNumber]// user or playlist to embed
       , { color: "000"
         , auto_play: true
         , maxwidth: 800
-        , maxheight: 1000
+        , maxheight: 300
         , show_comments: true } // options
       , document.getElementById("widget") // what element to attach player to
     );
+
   }
 
   $( "#button" ).click(function() {
-    getTracks();
+    $("#heart").show();
+    var genreSelection = $( "#genreSelect option:selected" ).text();
+    console.log(genreSelection);
+    getTracks(genreSelection);
   });
 
   $("a[rel*=leanModal]").leanModal({ closeButton: ".modal_close" });
@@ -96,4 +106,3 @@ $(function () {
   });
 
 });
-
